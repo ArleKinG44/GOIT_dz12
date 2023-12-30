63d9bac63f8f652df207c83642eac9a634aaa813
ArleKinG44 <denpolet@gmail.com>
2023-12-30 14:00:12 +0200

    action

--- main.py
+++ main.py
@@ -88,35 +88,132 @@ class AddressBook(UserDict):
         super().__init__()
         self.current_page = 0
 
+    def input_error(func):
+        def inner(*args, **kwargs):
+            try:
+                return func(*args, **kwargs)
+            except KeyError:
+                return "KeyError: The key you provided does not exist."
+            except ValueError:
+                return "ValueError: The value you provided is not valid."
+        return inner 
+
+    @input_error
     def add_record(self, record):
         self.data[record.name.value] = record
 
+    @input_error
     def find(self, name):
         return self.data.get(name)
 
+    @input_error
     def delete(self, name):
         if name in self.data:
             del self.data[name]
+        return f"Contact {name} deleted"
 
+    @input_error
     def iterator(self, n):
         self.current_page = 0
-        self.page_size = n
+        self.page_size = int(n)
         while self.current_page < len(self.data):
-            yield list(self.data.items())[self.current_page:self.current_page + self.page_size]
+            yield [(name, [phone.value for phone in record.phones])
+                   for name, record in list(self.data.items())
+                   [self.current_page:self.current_page + self.page_size]]
             self.current_page += self.page_size
 
+    @input_error
     def save_to_file(self, filename):
         with open(filename, 'wb') as file:
             pickle.dump(self.data, file)
+        return f"Data saved to {filename}"
 
+    @input_error
     def load_from_file(self, filename):
         with open(filename, 'rb') as file:
             self.data = pickle.load(file)
+        return f"Downloaded from {filename}"
 
+    @input_error
     def search(self, query):
         result = []
-        for record in self.data.values():
-            if query in record.name.value or any(query in phone.value for phone in record.phones):
-                result.append(record)
+        for name, record in self.data.items():
+            if query in name or any(query in phone.value for phone in record.phones):
+                result.append((name, [phone.value for phone in record.phones]))
         return result
-    
+
+    @input_error
+    def add_record_str(self, data):
+        name, phone = data.split()
+        record = Record(name)
+        record.add_phone(phone)
+        self.add_record(record)
+        return f"Contact {name} added"
+
+    @input_error
+    def edit_phone(self, data):
+        name, old_phone, new_phone = data.split()
+        record = self.find(name)
+        if record:
+            record.edit_phone(old_phone, new_phone)
+            return f"Phone number for {name} changed."
+        else:
+            return "The contact does not exist."
+
+    @input_error
+    def good_bye(self):
+        return "Good bye!"
+
+    @input_error
+    def start_iterator(self, n):
+        self.iterator_instance = self.iterator(int(n))
+        return next(self.iterator_instance, "No more records.")
+
+    @input_error
+    def next_page(self):
+        return next(self.iterator_instance, "No more records.")
+
+    @input_error
+    def choice_action(self, data):
+        parts = data.split()
+        if not parts:
+            return "No command given", None
+        command = parts[0]
+        args = ' '.join(parts[1:])
+        if command in self.ACTIONS:
+            return self.ACTIONS[command], args
+        else:
+            return "Give me a correct command please", None
+
+    def main(self):
+        self.ACTIONS = {
+            'add': self.add_record_str,
+            'delete': self.delete,
+            'edit_phone': self.edit_phone,
+            'find': self.find,
+            'show_all': self.start_iterator,
+            'next': self.next_page,
+            'search': self.search,
+            'save': self.save_to_file,
+            'load': self.load_from_file,
+            'close': self.good_bye,
+            'exit': self.good_bye,
+            '.': self.good_bye
+        }
+        while True:
+            data = input()
+            func, args = self.choice_action(data)
+            if isinstance(func, str):
+                print(func)
+                if func == "Good bye!":
+                    break
+            else:
+                result = func(args) if args else func()
+                print(result)
+                if result == "Good bye!":
+                    break
+
+
+if __name__ == "__main__":
+    ab = AddressBook()
+    ab.main()
